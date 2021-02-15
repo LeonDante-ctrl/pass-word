1f16397cfcfa65ea407ed44281614ab5e484c212
Leon Dante <maxgamerdu@gmail.com>
2021-02-15 08:34:36 +0300

    defined methods in my user class that talk about deleting and saving contacts and on my credentials class i added a linking factor to the empty array that i made earlier

--- user.py
+++ user.py
@@ -3,15 +3,31 @@ class User:
     Class that will generate new instances of users
     """
 
-    user_detail = []  # empty array
+    user_detail = []
 
-    def __init__(self, login_username):
-        """
-        the __init__method helps us define properties for our object self.
-        """
-
-        self.login_username = login_username
+    def save_detail(self):
 
         """
-        arguments for our __init__method will include the following.
+        the method saves detail objects into the empty user_detail array
         """
+        User.user_detail.append(self)
+
+    def delete_detail():
+
+        """
+        the delete_detail method is used to remove objects from the user detail array
+        """
+
+        User.user_detail.remove(self)
+
+    def __init__(self, account_name, username, password, confirm_password):
+
+        """
+        the __init__method helps us define properties for our objectsself.
+        """
+
+        self.account_name = account_name
+        self.username = username
+        self.password =password
+        self.confirm_password = confirm_password
+
