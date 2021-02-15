02f3df0d29dd89d9e5300120cbe3c3edb763c360
Leon Dante <maxgamerdu@gmail.com>
2021-02-15 07:50:11 +0300

    created my second class that defines the format of which the users credentials would be relevant to the method of input

---
+++ credentials.py
@@ -0,0 +1,18 @@
+class Credentials:
+    """
+    Class that will generate new instances of credentials
+    """
+
+
+
+    def __init__(self,login_credential):
+
+        """
+        the __init__method helps us define properties for our objects.
+        """
+
+        self.login_credential = login_credential
+
+        """
+        arguments for our __init__method will include the following.
+        """
