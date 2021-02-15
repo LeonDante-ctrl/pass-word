7e049882de1e81f5a76da2899f24cef5fd5caadc
Leon Dante <maxgamerdu@gmail.com>
2021-02-15 10:35:18 +0300

    input some methods in my run folder for unit testing successfully . applied the use of docstrings as well

--- run.py
+++ run.py
@@ -1,2 +1,38 @@
 from user import User
-from  credentials import Credential
+from credentials import Credential
+import random
+import string
+
+dash = '-' * 60
+
+
+def create_account(account_name, username, password, confirm_password):
+    """
+    function to create a new account
+    """
+
+    new_user = User(account_name, username, password, confirm_password)
+
+    return new_user
+
+
+def save_details(user):
+    """
+    function to save save_details
+    """
+    user.save_detail()
+
+
+def display_all_details():
+    """
+    function used to return all saved save_details
+    """
+    return User.display_all_details()
+
+
+def check_existing_user(username):
+    """
+    a function that is used to check and return all exissting accounts
+    """
+
+    return User.user_exist(username)
