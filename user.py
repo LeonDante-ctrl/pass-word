0f3bb08c83bcb015c16ecb11596e548e37136697
Leon Dante <maxgamerdu@gmail.com>
2021-02-15 07:42:12 +0300

    added in my user class as well as defined the initializing parameters

---
+++ user.py
@@ -0,0 +1,16 @@
+class User:
+    """
+    Class that will generate new instances of users
+    """
+
+    def __init__(self,login_username):
+
+        """
+        the __init__method helps us define properties for our object self.
+        """
+
+        self.login_username = login_username
+
+        """
+        arguments for our __init__method will include the following.
+        """
