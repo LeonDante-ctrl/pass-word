f8a5b1e8bb90d64ac203ab210b5063196ed43a66
Leon Dante <maxgamerdu@gmail.com>
2021-02-16 07:53:27 +0300

    Put in readme as well as code to run the file

---
+++ README.md
@@ -0,0 +1,75 @@
+# PASSWORD ENCRYPTER
+
+This is a Python project that safely encrypts your login credentials
+
+## Author name
+
+[Leon Baraka](https://github.com/LeonDante-ctrl/pass-word.git)
+
+## Project Description
+
+This is an app that allows users to store account credentials, it also gives them the chance to create new accounts and store their credentials, they can also delete those credentials that they don't need any more.
+
+## Technologies Used
+
+Python 3.8
+
+## Application requirements
+
+1. Ensure you have Python3.8 installed in your computer. you can download it by running this command
+
+`$ sudo apt-get update sudo apt-get install python3.8.`
+
+2. Ensure you have PiP installed in your computer, you can download it by running this command:
+
+`$ apt install python3-pip`
+
+## Project setup instruction/ installations
+
+
+1.  Copy the clone command.
+
+2.  From a terminal window, change to the local directory where you want to clone your repository.
+
+or just use this
+
+`$ git clone https://github.com/LeonDante-ctrl/pass-word.git`
+
+3. Run the following command to make the app executable;
+
+`$ chmod +x run.py`
+
+4. Run this command to open the app
+
+`$ ./run.py`
+
+
+## BDD
+
+| Behavior        | Result |
+| ------------- |:----:|
+| User types in 1 when prompted | User is prompted to input account details |
+| User types in 2 | are able to view all accounts created by him/her|
+| User types in 3 | is able to search for saved accounts using their username|
+| User types in 4 | The application closes|
+
+## TDD
+
+-To test the app, run this commands in the terminal;
+
+`$ python3.8 user_test.py`
+
+`$ python3.8 credential_test.py`
+
+
+## Contact Information
+
+Email-(maxgamerdu@gmail.com)
+
+Github user name -Dante
+
+## License
+
+MIT License
+
+Copyright (C) [2021] [Leon Baraka]
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from getpass import getpass
+
 from user import User
 from credentials import Credential
 import random
@@ -72,3 +74,144 @@ def main():
     print(f"Hello {user_name}, Behold!!!, your password manager")
 
     print('\n')
+
+    while True:
+
+        list: str = ('''
+            1 - Register a new account
+            2 - Display accounts
+            3 - Find accounts
+            4 - Exit the locker\n''')
+        print(list)
+
+        short_code = input().lower()
+
+        if short_code == '1':
+
+            print(f"{user_name} Fill in some input")
+
+            print("-" * 10)
+
+            print("User name")
+
+            account_name = input()
+
+            print('\n')
+
+            print("Username")
+            username = input()
+
+            print('\n')
+
+            print("Need a randomly generated password?")
+
+            print("yes", "no")
+            ans = input().lower()
+
+            if ans == 'yes':
+
+                print(generatePassword(10))
+
+                save_details(create_account(account_name, username, password, confirm_password))
+
+                print('\n')
+
+                print(f"{user_name} {account_name} account of {username} created and password saved")
+
+                print('\n')
+
+
+            elif ans == 'no':
+
+                password = getpass('password:')
+                print("*********")
+
+                confirm_password = getpass('confirm password:')
+                print("*********")
+
+                save_details(create_account(account_name, username, password, confirm_password))
+
+                print('\n')
+
+                print(dash)
+
+                print(f"Hey {user_name}")
+                print(f"Your account name is {account_name}.com")
+                print(f"Your username is {username}")
+                print("encrypted passwords can be viewed through the option 2 / 3")
+
+                print(dash)
+
+                print('\n')
+
+                print(f"{user_name} what action do you want to perform?")
+
+        elif short_code == '2':
+
+            if display_all_details():
+
+                print(f"{user_name} preview all the saved accounts")
+
+                print('\n')
+
+                for user in display_all_details():
+                    print(dash)
+
+                    print(f"Account is {user.account_name}.com")
+                    print(f"Account username is {user.username}")
+                    print(f"The account's password is {user.password} keep this private")
+
+                    print(dash)
+
+                    print('\n')
+
+                    print(f"{user_name} what action do you want to perform?")
+
+            else:
+
+                print('\n')
+
+                print(f" {user_name} nothing saved just yet")
+
+                print('\n')
+
+                print(f"{user_name} what action do you want to perform?")
+
+        elif short_code == '3':
+
+            print(f"{user_name} Search for username here")
+
+            search_username = input()
+
+            if check_existing_user(search_username):
+
+                search_username = find_user(search_username)
+
+                print(dash)
+
+                print(f"Account is {search_username.account_name}.com")
+                print(f"Account username is {search_username.username}")
+                print(f"Account password is {search_username.password} keep password private")
+
+                print(dash)
+
+                print(f"{user_name} what action do you want to perform?")
+
+            else:
+                print(f"{user_name} does ot exist")
+
+                print(f"{user_name} what action do you want to perform?")
+
+        elif short_code == "4":
+
+            print("Cheers.")
+
+            break
+
+        else:
+            print("Illegible , try that again")
+
+            print(f"{user_name} what action do you want to perform?")
+
+    if __name__ == '__main__':
+        main()
